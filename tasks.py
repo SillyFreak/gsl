baa0dac24040e14ba276f41be4efa21a92b4706e
SillyFreak <sillyfreak.ck@gmail.com>
2017-11-17 22:26:23 +0100

    move grammar file to actual Python module

--- tasks.py
+++ tasks.py
@@ -2,4 +2,4 @@ from invoke import run, task
 
 @task
 def antlr4(context):
-    run("antlr4 -Dlanguage=Python3 -package gsl.grammar -visitor -no-listener -o gsl grammar/GSL.g4")
+    run("antlr4 -Dlanguage=Python3 -package gsl.grammar -visitor -no-listener gsl/grammar/GSL.g4")
