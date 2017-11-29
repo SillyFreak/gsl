c82b141dfb600b2e43667ae0e16ac5c5281d47c0
SillyFreak <sillyfreak.ck@gmail.com>
2017-11-29 10:15:03 +0100

    change tasks to have one target that creates the g4v visitor; it does a git reset!

--- tasks.py
+++ tasks.py
@@ -1,11 +1,9 @@
 from invoke import run, task
 
 @task
-def g4v_antlr(context):
+def g4v(context):
     run("antlr4 -Dlanguage=Python3 -package gsl.grammar -visitor -no-listener gsl/grammar/G4Visitor.g4")
-
-@task
-def g4v_g4v(context):
+    run("git checkout gsl/grammar/G4VisitorVisitor.py")
     run("g4v gsl/grammar/G4VisitorVisitor.g4v")
 
 @task
