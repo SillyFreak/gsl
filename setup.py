08c4b313186e0c2554881483c1456e5b5bf049a2
SillyFreak <sillyfreak.ck@gmail.com>
2018-05-28 17:22:26 +0200

    update Python classifier to 3.6

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='code generation',
